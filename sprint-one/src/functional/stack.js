a304f1e522bf74d27572e5867a4dc799a8e9c76f
Hack Reactor Student <student@hackreactor.com>
2019-04-10 14:52:08 -0700

    Stack works (hallejuah)

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,33 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  storage.currentSize = 0;
+  storage.currentNumber = 0;
+  storage.lastPopped;
   // Implement the methods below
   someInstance.push = function(value) {
+    // need some way to programmatically generate numbers
+    storage.currentNumber += 1;
+    storage.currentSize += 1;
+    storage[storage.currentNumber] = value;
   };
 
   someInstance.pop = function() {
+    if (storage.currentSize !== 0) {
+      storage.lastPopped = storage[storage.currentNumber];
+      delete storage[storage.currentNumber];
+      storage.currentSize -= 1;
+      storage.currentNumber -= 1;
+    } else {
+      
+    }
+    return storage.lastPopped;
+
+    
   };
 
   someInstance.size = function() {
+    return storage.currentSize;
   };
 
   return someInstance;
