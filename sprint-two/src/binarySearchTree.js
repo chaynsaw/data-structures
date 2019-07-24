16a3da56436286a3c0f13b40e37e07621e7d9afb
Chaynor Hsiao <chaynsaw@gmail.com>
2019-07-24 14:28:02 -0700

    Implement basic BST methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,21 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = Object.create(bst);
+  binaryTree.left = null;
+  binaryTree.right = null;
+  binaryTree.value = value;
+  return binaryTree;
 };
 
+var bst = {};
 
+bst.insert = () => {
+}
+
+bst.contains = () => {
+}
+
+bst.depthFirstLog = () => {
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
